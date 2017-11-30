e9566c04d7d3c7bdb87d2f27be9af19c20d1dece
mjones <mikedeanjones@yahoo.com>
2017-11-30 15:43:52 -0500

    Created a script for creating test data. Scrambles sequences.

---
+++ scripts/python/create_test_data.py
@@ -0,0 +1,136 @@
+#!/usr/bin/env python
+
+__author__ = 'JONESMIC'
+
+#!/usr/bin/env python
+# ==============================================================================
+#
+#
+# @author: Michael Jones
+# @date:   11/30/17�
+# ==============================================================================#
+'''
+
+ Create test data from real data. 
+
+'''
+
+import re
+import random
+import argparse
+import pandas as pd
+import os
+import subprocess
+
+ip2_pept_pattern = re.compile(r'(\w+)(C\(\d+\.\d+\))(\w+)')
+
+def scramble_string(word):
+    word = list(word)
+    random.shuffle(word)
+    return ''.join(word)
+
+def scramble_ip2(ip2_pep):
+    match = re.match(ip2_pept_pattern, ip2_pep)
+    scrambled_pep = ''.format(scramble_string(match.group(1)), match.group(2), match.group(3))
+    return scrambled_pep
+
+def scramble_protein(protein):
+    is_decoy = protein.contains('Reverse_tr')
+    protein = protein.replace('Reverse_tr', '')
+
+    words = protein.split(r'[|\s]')
+
+    scrambled_prot = []
+
+    if is_decoy:
+        scrambled_prot.append('Reverse_tr')
+
+
+    for word in words:
+        scrambled_prot.append(scramble_string(word))
+    return ''.join(scrambled_prot)
+
+def create_filtered_data(peptide_list_file, sequence_patterns_file, file_type):
+    df = None
+    if file_type == 'CSV':
+        df = pd.read_csv(peptide_list_file)
+    elif file_type == 'TXT':
+        df = pd.read_table(peptide_list_file, skiprows=24)
+
+    patterns = pd.read_table(sequence_patterns_file, header=None)
+
+    frames = []
+    for pattern in patterns.iloc[:, 0]:
+        frames.append(df.filter(regex=pattern))
+
+    df = pd.concat(frames)
+    sequence_col = None
+    protein_col = None
+    if file_type == 'CSV':
+        sequence_col = 'SEQUENCE'
+        protein_col = 'PROTEIN'
+    elif file_type == 'TXT':
+        sequence_col = 'sequence'
+        protein_col = 'protein'
+
+    df[sequence_col] = df[sequence_col].apply(scramble_ip2)
+    df[protein_col] = df[protein_col].apply(scramble_protein)
+
+
+
+    return df
+
+
+def process_with_analyzeQuantComapare(out_dir, peptide_list_file):
+
+    python_bin = 'python'
+    analyze_quant_comapare = './scripts/nomurarg-scripts/analyze_quantCompare.py'
+
+    in_file = '{0}/{1}'.format(out_dir, peptide_list_file)
+    out_file = '{0}/{1}'.format(out_dir, "results.csv")
+    ## "${PYTHON} ${ANALYZE_QUANT_SCRIPT} ${TEST_DATA_DIR}/peptideList.csv ${TEST_DATA_DIR}/results.csv"
+    cmd = [python_bin, analyze_quant_comapare, in_file, out_file]
+    print(subprocess.check_output(cmd))
+
+    out_file = '{0}/{1}'.format(out_dir, "resultsverbose.csv")
+    ## "${PYTHON} ${ANALYZE_QUANT_SCRIPT} -v ${TEST_DATA_DIR}/peptideList.csv ${TEST_DATA_DIR}/resultsverbose.csv"
+    cmd = [python_bin, analyze_quant_comapare, '-v', in_file, out_file]
+    print(subprocess.check_output(cmd))
+
+
+def main(out_dir, peptide_list_file, sequence_patterns_file):
+    file_type = None
+    if peptide_list_file.endswith('csv'):
+        file_type = 'CSV'
+    elif peptide_list_file.endswith('csv'):
+        file_type = 'TXT'
+
+    df = create_filtered_data(out_dir, peptide_list_file, sequence_patterns_file, file_type)
+
+    file_path = "{0}/{1}".format(out_dir, os.path.basename(peptide_list_file))
+
+    ## Write file
+    if file_type == 'CSV':
+        df.to_csv(file_path, index=False)
+    elif file_type == 'TXT':
+        df.to_csv(file_path, index=False, sep="\t")
+
+    process_with_analyzeQuantComapare(out_dir, os.path.basename(peptide_list_file))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Create test data.')
+
+    parser.add_argument('--out_dir', type=str, required=True,
+                        help='The directory to put the test data into')
+
+    parser.add_argument('--peptide_list_file', type=str, required=True,
+                        help='A peptideList.csv AKA quant_stat_peptide_compare.txt')
+
+    parser.add_argument('--sequence_patterns', type=str, required=True,
+                        help='A file listing the IP2 format pep patterns. Example: "ICD\([0-9]+.[0-9]+\)LLLLLEK"')
+
+    args = parser.parse_args()
+
+    main(args.root_dir, args.peptide_list_file, args.sequence_patterns)
+
